9e51301238583253f9fbc1e37381de6ea5d82753
Danny Martini <danny@wunderflats.com>
2018-07-28 16:27:54 +0200

    finish functionality

--- spec/md5sum-spec.js
+++ spec/md5sum-spec.js
@@ -1,72 +1,43 @@
-'use babel';
+"use babel";
 
-import Md5sum from '../lib/md5sum';
+import Md5sum from "../lib/md5sum";
+import md5 from "md5";
+// import delay from "delay";
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('Md5sum', () => {
+const delay = function(timeout) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, timeout);
+  });
+};
+
+describe("Md5sum", () => {
   let workspaceElement, activationPromise;
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('md5sum');
+    activationPromise = atom.packages.activatePackage("md5sum");
   });
 
-  describe('when the md5sum:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.md5sum')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'md5sum:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.md5sum')).toExist();
-
-        let md5sumElement = workspaceElement.querySelector('.md5sum');
-        expect(md5sumElement).toExist();
-
-        let md5sumPanel = atom.workspace.panelForItem(md5sumElement);
-        expect(md5sumPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'md5sum:toggle');
-        expect(md5sumPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.md5sum')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'md5sum:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let md5sumElement = workspaceElement.querySelector('.md5sum');
-        expect(md5sumElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'md5sum:toggle');
-        expect(md5sumElement).not.toBeVisible();
+  describe("when the md5sum:hash event is triggered", () => {
+    fit("hashed whatever is selected", function() {
+      waitsForPromise(async () => {
+        // SETUP
+        const editor = await atom.workspace.open();
+        editor.insertText("foo");
+        expect(editor.getText()).toBe("foo");
+        editor.selectAll();
+        
+        // TEST
+        await atom.commands.dispatch(workspaceElement, "md5sum:hash");
+        await activationPromise;
+        
+        // ASSERT
+        expect(editor.getText()).toBe("acbd18db4cc2f85cedef654fccc4a4d8");
       });
     });
   });
--- spec/md5sum-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Md5sumView from '../lib/md5sum-view';
-
-describe('Md5sumView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
